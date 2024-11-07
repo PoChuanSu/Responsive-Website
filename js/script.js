08a5253219f737afade2af4adc9e2f05b52b42ab
PoChuanSu <peterpeter1010@live.com>
2024-11-08 10:54:23 +1100

    sticky bar

--- js/script.js
+++ js/script.js
@@ -17,6 +17,64 @@ btnNavEl.addEventListener('click', function(){
 })
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver (
+  function(entries){
+    const ent = entries[0];
+    if (ent.isIntersecting === false)
+    {
+      console.log(ent);
+      document.body.classList.add('sticky');
+    }
+    
+    if (ent.isIntersecting === true)
+    {
+      console.log(ent);
+      document.body.classList.remove('sticky');
+    }
+},
+{
+  // In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin:'-80px',
+}
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function(link) {
+  link.addEventListener('click',function(e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    // console.log(href);
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({
+      top:0,
+      behavior:"smooth",
+    })
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView ({behavior:"smooth"});
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle("nav-open");
+    }
+  })
+})
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
